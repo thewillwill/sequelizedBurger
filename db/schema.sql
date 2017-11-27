2b9d78334d7192f3d44338abfacfebe95006bf80
William Brooks <brooks.william@gmail.com>
2017-11-26 19:52:37 -0800

    changed production config

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,3 @@
 ### Schema
 
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	ingredients varchar(255) NOT NULL,	
-	devoured BOOLEAN DEFAULT false,
-	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (id)
-);
+CREATE DATABASE sequelizedBurger;
